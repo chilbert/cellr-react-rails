3539cd980c1a186e39610f0f0608c159427bb7e1
chilbert <chase@chasehilbert.com>
2020-01-16 20:55:17 -0600

    Updating the 404 Page

--- cellr-app/src/components/pageComponents/NotFound.js
+++ cellr-app/src/components/pageComponents/NotFound.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import LazyHero from 'react-lazy-hero';
 
 const NotFound = () => (
-    <div>
-        <h2>Page Not Found</h2>
-    </div>
+    <LazyHero imageSrc="https://unsplash.it/2000/1000">
+         <h1>OOPS! Cork It Up!</h1>
+         <h3> This page doesn't exist!</h3>
+    </LazyHero>
+
 )
 
 export default NotFound
